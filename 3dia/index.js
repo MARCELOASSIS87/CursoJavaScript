da47e1287315b79d98554362a165934ad418b3b0
Marcelo Assis <Marceloassis50@gmail.com>
2022-10-03 11:00:44 -0300

    factory

---
+++ 3dia/index.js
@@ -0,0 +1,32 @@
+// Factory
+
+function createBook(title, author, pages) {
+    const book = {
+        bookTitle: title,
+        bookAuthor: author,
+        bookPages: pages,
+        printBook: function () {
+            for (let i = 0; i <= book.bookPages; i++) {
+                console.log('printing page ', i)
+            }
+        }
+    }
+
+    return book
+}
+
+const book1 = createBook('atomic habbit', 'james', 306)
+const book2 = createBook('Zé biloca', 'José Bill', 100)
+
+book1.color = 'green'
+console.log(book1)
+console.log(book2)
+// book.printBook() 
+
+// function printBook(bookPages) {
+//     for (let i = 0; i <= bookPages; i++){
+//         console.log('printing page ', i)
+//     }        
+// }
+// printBook(book.bookPages)
+// console.log(book)  
