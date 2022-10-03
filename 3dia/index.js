e2a8f20cad231450ee5361c2691204644326b988
Marcelo Assis <Marceloassis50@gmail.com>
2022-10-03 11:06:35 -0300

    Constructors

--- 3dia/index.js
+++ 3dia/index.js
@@ -1,26 +1,26 @@
 // Factory
 
-function createBook(title, author, pages) {
-    const book = {
-        bookTitle: title,
-        bookAuthor: author,
-        bookPages: pages,
-        printBook: function () {
-            for (let i = 0; i <= book.bookPages; i++) {
-                console.log('printing page ', i)
-            }
-        }
-    }
+// function createBook(title, author, pages) {
+//     const book = {
+//         bookTitle: title,
+//         bookAuthor: author,
+//         bookPages: pages,
+//         printBook: function () {
+//             for (let i = 0; i <= book.bookPages; i++) {
+//                 console.log('printing page ', i)
+//             }
+//         }
+//     }
 
-    return book
-}
+//     return book
+// }
 
-const book1 = createBook('atomic habbit', 'james', 306)
-const book2 = createBook('Zé biloca', 'José Bill', 100)
+// const book1 = createBook('atomic habbit', 'james', 306)
+// const book2 = createBook('Zé biloca', 'José Bill', 100)
 
-book1.color = 'green'
-console.log(book1)
-console.log(book2)
+// book1.color = 'green'
+// console.log(book1)
+// console.log(book2)
 // book.printBook() 
 
 // function printBook(bookPages) {
@@ -30,3 +30,16 @@ console.log(book2)
 // }
 // printBook(book.bookPages)
 // console.log(book)  
+
+// Constructor
+function CreateBook(title, author, pages) {
+
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages
+
+}
+
+const book1 = new CreateBook('atomic habbit', 'james', 306)
+
+console.log(book1)
